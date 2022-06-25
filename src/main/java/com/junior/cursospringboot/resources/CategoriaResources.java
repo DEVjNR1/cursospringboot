bd23efdb7a74dc4d37c26df9c5b7a468fbda87ef
Geziel Junior <geziel.junior@hotmail.com>
2022-06-25 18:53:33 -0300

    Rest Testado

---
+++ src/main/java/com/junior/cursospringboot/resources/CategoriaResources.java
@@ -0,0 +1,20 @@
+package com.junior.cursospringboot.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResources {
+
+	@RequestMapping
+public String listar() {
+	return " REST está funcionando!";
+}
+	
+	
+	
+}
