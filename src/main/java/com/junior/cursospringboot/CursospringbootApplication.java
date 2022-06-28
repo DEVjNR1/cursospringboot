a9ddcdf8eb61a5bdf176a0a01d78ba014812f2e1
Geziel Junior <geziel.junior@hotmail.com>
2022-06-27 21:13:57 -0300

    criando operação de instaciação

--- src/main/java/com/junior/cursospringboot/CursospringbootApplication.java
+++ src/main/java/com/junior/cursospringboot/CursospringbootApplication.java
@@ -1,13 +1,38 @@
 package com.junior.cursospringboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.junior.cursospringboot.domain.Categoria;
+import com.junior.cursospringboot.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursospringbootApplication {
+public class CursospringbootApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+	
+	
+	
+	
 
 }
